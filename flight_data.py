4b77bea72a2fde69bc37e763c2bc15467005f973
Shantanu Mahato <62724719+ShanuXD@users.noreply.github.com>
2021-01-25 14:12:21 +0530

    Update flight_data.py

--- flight_data.py
+++ flight_data.py
@@ -1,7 +1,7 @@
 
 class FlightData:
 
-    def __init__(self, price, base_city, base_airport, location_city, location_airport, out_date, return_date):
+    def __init__(self, price, base_city, base_airport, location_city, location_airport, out_date, return_date, stop_overs, via_city):
         self.price = price
         self.base_city = base_city
         self.base_airport = base_airport
@@ -9,3 +9,5 @@ class FlightData:
         self.location_airport = location_airport
         self.out_date = out_date
         self.return_date = return_date
+        self.stop_overs = stop_overs
+        self.via_city = via_city
