adb859ffc46337faf78e615a5b6975b752970cd7
Shantanu Mahato <62724719+ShanuXD@users.noreply.github.com>
2021-01-25 14:09:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from data_manager import DataManager
 from flight_search import FlightSearch
 from datetime import datetime, timedelta
 from notification_manager import NotificationManager
+from users import Users
 
 
 base_city_iata = "BLR" #Banglore
@@ -10,17 +11,17 @@ base_city_iata = "BLR" #Banglore
 data_manager = DataManager()
 flight_search = FlightSearch()
 notification_manager = NotificationManager()
+users = Users()
 sheet_data = data_manager.get_data()
 
-pprint(sheet_data)
+# pprint(sheet_data)
 
-for location in sheet_data:
-    if not location['iataCode']:
+if sheet_data[0]['iataCode'] == ' ':
+    for location in sheet_data:
         city_name = location['city']
         location["iataCode"] = flight_search.getIataCode(city_name)
     data_manager.location_data = sheet_data
     data_manager.updateLocationData()
-
 
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
@@ -32,9 +33,24 @@ for location in sheet_data:
         continue
     if flight.price < location['lowestPrice']:
 
-        msg = f"Low Price Alert!!! Only £{flight.price} to fly from {flight.base_city}-{flight.base_airport} to {flight.location_city}-{flight.location_airport}, from {flight.out_date} to {flight.return_date}."
+        msg = f"Low Price Alert!!! Only Rs.{flight.price} to fly from {flight.base_city}-{flight.base_airport} to {flight.location_city}-{flight.location_airport}, from {flight.out_date} to {flight.return_date}."
 
-        notification_manager.sendMsg(msg)
+        if flight.stop_overs > 0:
+            msg += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
+
+        # notification_manager.sendMsg(msg)
+        link = f"https://www.google.co.uk/flights?hl=en#flt={flight.base_airport}.{flight.location_airport}.{flight.out_date}*{flight.location_airport}.{flight.base_airport}.{flight.return_date}"
+
+        all_users_data = users.getUsers()
+        # print(all_users_data)
+        for user in all_users_data:
+            user_email = user['email']
+            notification_manager.sendMail(msg, user_email, link)
+
+
+
+
+
 
 
 
